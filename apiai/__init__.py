37bd9a4cfce6e05c3f9ec5239eec380692ebc56e
Dmitriy Kuragin <kuragin@speaktoit.com>
2016-03-04 18:01:04 +0600

    Entity & Entry class moved to query module

--- apiai/__init__.py
+++ apiai/__init__.py
@@ -25,14 +25,17 @@ __all__ = [
             'Entity'
             ]
 
+from .requests.query.query import Entry
+from .requests.query.query import Entity
+
 from .requests  import Request
 from .requests  import QueryRequest
 
 from .apiai     import ApiAI
 from .apiai     import TextRequest
 from .apiai     import VoiceRequest
-from .apiai     import Entry
-from .apiai     import Entity
+# from .apiai     import Entry
+# from .apiai     import Entity
 
 from .VAD       import VAD
 from .resampler import Resampler
