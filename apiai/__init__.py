6b8d54de522469a354fc5c15a998d3324b80b5ca
Dmitriy Kuragin <kuragin@speaktoit.com>
2016-12-09 22:36:22 +0700

    version 1.0.4

--- apiai/__init__.py
+++ apiai/__init__.py
@@ -28,7 +28,7 @@ __author__ = "Dmitriy Kuragin"
 __copyright__ = "Copyright 2016 api.ai"
 __contributors__ = []
 __license__ = "Apache 2.0"
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 
 __all__ = [
     'apiai',
